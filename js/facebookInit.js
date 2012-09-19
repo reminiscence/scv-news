065f85d1a49571c988a36b46ee52ad96c44a1aa6
jeonguk <newlife8837@gmail.com>
2012-09-19 13:27:25 +0900

    forth test

--- js/facebookInit.js
+++ js/facebookInit.js
@@ -1,18 +1,6 @@
  window.fbAsyncInit = function() {
- 	var $doc = $(window.body);
 	FB.init({appId: '524760984205471', status: true, cookie: true,
 		xfbml: true});
-
-	$doc.bind('checkLogin',function(){
-		//console.log("asdf");
-		FB.getLoginStatus(function(response) {
-			if (response.session) {
-				console.log("asdf");// logged in and connected user, someone you know
-			} else {
-				console.log("ddd");// no user session available, someone you dont know
-			}
-		});
-	});
 };
 
 	
