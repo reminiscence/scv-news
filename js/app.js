dc2d6ff593a49ac2d51a4ab075d1b06c1868a39a
jeonguk <newlife8837@gmail.com>
2012-09-19 13:23:32 +0900

    getLoginStatus third test

--- js/app.js
+++ js/app.js
@@ -233,15 +233,4 @@ $(function() {
 		headline.showNewsTitle(); //헤드라인 새로 뿌려줌
 	});
 
-	$doc.bind('checkLogin',function(){
-		//console.log("asdf");
-		FB.getLoginStatus(function(response) {
-			if (response.session) {
-				console.log("asdf");// logged in and connected user, someone you know
-			} else {
-				console.log("ddd");// no user session available, someone you dont know
-			}
-		});
-	});
-
 });
--- js/facebookInit.js
+++ js/facebookInit.js
@@ -1,10 +1,24 @@
  window.fbAsyncInit = function() {
-   FB.init({appId: '347722045320000', status: true, cookie: true,
-            xfbml: true});
- };
- (function() {
-   var e = document.createElement('script'); e.async = true;
-   e.src = document.location.protocol +
-     '//connect.facebook.net/ko_KR/all.js';
-   document.getElementById('fb-root').appendChild(e);
- }());
+ 	var $doc = $(window.body);
+	FB.init({appId: '524760984205471', status: true, cookie: true,
+		xfbml: true});
+
+	$doc.bind('checkLogin',function(){
+		//console.log("asdf");
+		FB.getLoginStatus(function(response) {
+			if (response.session) {
+				console.log("asdf");// logged in and connected user, someone you know
+			} else {
+				console.log("ddd");// no user session available, someone you dont know
+			}
+		});
+	});
+};
+
+	
+(function() {
+	var e = document.createElement('script'); e.async = true;
+	e.src = document.location.protocol +
+	'//connect.facebook.net/ko_KR/all.js';
+	document.getElementById('fb-root').appendChild(e);
+}());
