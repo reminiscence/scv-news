dc87e698da2baa5919fe1f2b9b5ab809cf278273
jeonguk <newlife8837@gmail.com>
2012-09-20 16:46:29 +0900

    test

--- js/DataStorage.js
+++ js/DataStorage.js
@@ -16,13 +16,13 @@ DataStorage.prototype.saveData = function(){
 };
 
 DataStorage.prototype.loadBookmarkData = function(){
-	console.log("start");
 	var ws = new cloudmine.WebService({
 	  appid: 'a53f225b5b9b465fac29085d6f98b18f',
 	  apikey: '02e619ec0ee34bccb975fca744e79717'
 	});
 
 	ws.get('bookmark').on('success', function(data, response){
+		console.log(data);
 		config.bookmark = data;
 	});
 };
