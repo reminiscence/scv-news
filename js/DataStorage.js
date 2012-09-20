a7f9c35f82b76234e7f8826d6404208b98d9f49c
jeonguk <newlife8837@gmail.com>
2012-09-20 12:40:55 +0900

    datastorage.js commit

---
+++ js/DataStorage.js
@@ -0,0 +1,16 @@
+function DataStorage(){
+
+}
+
+DataStorage.prototype.saveData = fucntion(){
+	var bookmark = config.bookmark;
+
+	var ws = new cloudmine.WebService({
+	  appid: '2e14ded867674f45b17a674eea31ad00',
+	  apikey: 'e1ff58f5b1634eff928f7c895388c57f'
+	});
+
+	ws.set(bookmark).on('success', function(data, response){
+		console.log(data);
+	});
+};
