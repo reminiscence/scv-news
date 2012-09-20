6f9cc8733a12605a0c98eb442c9e4af93b3e5488
jeonguk <newlife8837@gmail.com>
2012-09-20 17:39:39 +0900

    horizentalBox width 동적변환 추가

--- js/Bookmark.js
+++ js/Bookmark.js
@@ -10,6 +10,7 @@ Bookmark.prototype.showBookmarkList = function(){
 
 	$bookmarkBox.empty(); //이전 항목을 지움. 새로이 불러올 항목을 $.get 부분에서 새로 뿌려줌
 	$bookmarkBox.html('<div class="control_bar"><button type="button" class="close" id="closebookmarkBox" data-dismiss="modal" aria-hidden="true">×</button></div><div id="boxList"><div class="horizentalBox"></div></div>')
+	$('.horizentalBox').css('width', length*250+'px')
 	$.get('./jst/bookmark-template.jst',function(tmpl){
 		for(var i =0; i < length; i++){
 			news = bookmark[i];
