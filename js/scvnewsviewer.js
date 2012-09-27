751c14dfa010a9a31fe458c163bfb4916b50b70b
jeonguk <newlife8837@gmail.com>
2012-09-27 17:44:02 +0900

    test

--- js/scvnewsviewer.js
+++ js/scvnewsviewer.js
@@ -222,7 +222,7 @@ Bookmark.prototype.deleteBookmark = function(vid){
 					console.log(data,response);
 				});
 
-				ws.destroy('bookmark',{'newsList[0]'}).on('error',function(data,response){
+				ws.destroy('vid = "'+vid+ '"').on('error',function(data,response){
 					console.log("failed");
 					console.log(data,response);
 				});
