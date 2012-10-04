cb6ec2bdee9b842b5fd210b4ee41b43d711cd2d0
jeonguk <newlife8837@gmail.com>
2012-10-04 16:01:30 +0900

    test

--- js/facebookInit.js
+++ js/facebookInit.js
@@ -2,3 +2,11 @@ window.fbAsyncInit = function() {
 	FB.init({appId: '524760984205471', channelUrl : './channel.html', status: true, cookie: true,
 		xfbml: true});
 };
+
+(function(d){
+	var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+	if (d.getElementById(id)) {return;}
+	js = d.createElement('script'); js.id = id; js.async = true;
+	js.src = "//connect.facebook.net/ko_KR/all.js";
+	ref.parentNode.insertBefore(js, ref);
+}(document));
