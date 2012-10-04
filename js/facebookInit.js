012f75ed0fbfc2d997da0f2a6346a65a5e5073a0
jeonguk <newlife8837@gmail.com>
2012-10-04 17:05:40 +0900

    test

--- js/facebookInit.js
+++ js/facebookInit.js
@@ -1,6 +1,6 @@
 window.fbAsyncInit = function() {
 	FB.init({appId: '524760984205471', 
-		channelUrl : './channel.html', 
+		channelUrl : 'channel.html', 
 		status: true, 
 		cookie: true, 
 		xfbml: true,  
