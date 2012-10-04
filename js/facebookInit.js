548ec368a9b76380d52de041cc502e568d389f21
jeonguk <newlife8837@gmail.com>
2012-10-04 17:06:33 +0900

    test

--- js/facebookInit.js
+++ js/facebookInit.js
@@ -1,6 +1,6 @@
 window.fbAsyncInit = function() {
 	FB.init({appId: '524760984205471', 
-		channelUrl : 'channel.html', 
+		channelUrl : 'scv.mistrie.pe.kr/channel.html', 
 		status: true, 
 		cookie: true, 
 		xfbml: true,  
