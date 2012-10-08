bf9d3c66045c42789cc0986a8597a6ea63e67ad7
jeonguk <newlife8837@gmail.com>
2012-10-08 15:38:27 +0900

    test

--- js/app.js
+++ js/app.js
@@ -1,20 +1,3 @@
-window.fbAsyncInit = function() {
-	FB.init({appId: '524760984205471', 
-		channelUrl : 'scv.mistrie.pe.kr/channel.html', 
-		status: true, 
-		cookie: true, 
-		xfbml: true
-	});
-};
-
-(function(d){
-	var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-	if (d.getElementById(id)) {return;}
-	js = d.createElement('script'); js.id = id; js.async = true;
-	js.src = "//connect.facebook.net/ko_KR/all.js";
-	ref.parentNode.insertBefore(js, ref);
-}(document));
-
 $(function() {
 
 	var $doc =$(document.body);
