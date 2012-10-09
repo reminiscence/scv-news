2bad39f43394b3261140dfd8e2e6d85378cbfab2
jeonguk <newlife8837@gmail.com>
2012-10-09 14:08:33 +0900

    test

--- js/scvnewsviewer.js
+++ js/scvnewsviewer.js
@@ -181,7 +181,8 @@ Bookmark.prototype.showBookmarkList = function(){
 	}
 	
 	$bookmarkBox.empty(); //이전 항목을 지움. 새로이 불러올 항목을 $.get 부분에서 새로 뿌려줌
-	$bookmarkBox.html('<div class="list-title">Book mark</div><button type="button" class="close" id="closebookmarkBox" data-dismiss="modal" aria-hidden="true">×</button>');
+	$bookmarkBox.html('<div class="title-bar"><div class="list-title">Book mark</div><button type="button" class="close" id="closebookmarkBox" data-dismiss="modal" aria-hidden="true">×</button></div>');
+	$bookmarkBox.append('<div class="boxlist>');
 	$.get('./jst/bookmark-template.jst',function(tmpl){
 		for(var i =0; i < length; i++){
 			news = bookmark[i];
