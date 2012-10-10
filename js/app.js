f85d92ff6c7b157c75d1c3e11ef7b3d297a1cc13
jeonguk <newlife8837@gmail.com>
2012-10-10 11:31:38 +0900

    test

--- js/app.js
+++ js/app.js
@@ -38,7 +38,7 @@ $(function() {
 		config.autoPlay = true;
 	} else {
 		config.autoPlay = cookie;
-		console.log(cookie, config.autoPlay);
+		console.log(typeof cookie, config.autoPlay);
 		if(!cookie){
 			configBox.setAutoPlay(cookie);
 		}
