b312b6ed988ae8fc5a5fdb8bc217e8e2a041d6ec
jeonguk <newlife8837@gmail.com>
2012-10-10 12:59:49 +0900

    test

--- js/scvnewsviewer.js
+++ js/scvnewsviewer.js
@@ -269,22 +269,20 @@ ConfigBox.prototype.init = function(){
 
 ConfigBox.prototype.setAutoPlay = function(cookie){
 	var $active = $('.active');
-	console.log(cookie);
 	if(cookie == null){
 		if($active.text()=="ON"){
 			config.autoPlay = true;
 		} else {
 			config.autoPlay = false;
 		}
-		$active.button('toggle');
+		//$active.button('toggle');
 	} else {
 		config.autoPlay = false;
-		$active.button('toggle');
+		//$active.button('toggle');
 	}
 	
 	//cookie에 autoPlay 설정 값 저장. expires는 1주일(7일)
 	$.cookie('autoPlay', config.autoPlay, {expires : 7, path : '/'});
-	console.log($.cookie('autoPlay'));
 };
 
 function DataLoader(){
